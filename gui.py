cedf2dc0cdeb60b93f365d702b4737531b8ae053
Samantha Hartzell <samhartz14@gmail.com>
2019-03-21 10:39:55 -0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,26 +1,33 @@
-import Tkinter as tk
-import tkFileDialog
-from defs import *
+try:
+    import tkinter as tk
+except ImportError:
+    import Tkinter as tk
+# import tkinter as tk
+try:
+    import tkFileDialog as filedialog
+except ImportError:
+    import tkinter.filedialog as filedialog
+from defs_Feb15 import *
 
 root = tk.Tk()  # This creates a window, but it won't show up
 
 # Default settings
 weatherOption = "ACTUAL"  # options: ACTUAL, AVG, BLM, STEP, CONST, VPD
 lightOption = "ACTUAL" # options: ACTUAL, STEP, PAR
-durationInp = tk.IntVar(value = 90) # days (must be an integer)
+durationInp = tk.IntVar(value = 10) # days (must be an integer)
 sInit = tk.DoubleVar(value = 0.4)
 soilVariable = tk.StringVar(value="Sandy loam") # default value
 capVariable = tk.StringVar(value = "Yes")
 spVariable = tk.StringVar(value = "Opuntia ficus-indica") # default value
 rainVariable = tk.StringVar(value = "Drydown") # options: "Drydown", "Constant", "Input rainfall", "Stochastic", "Input VWC"
-weatherFile = tk.StringVar(value="sample_data\TempleApril2015Interp30.xlsx") # default value for the location of the weather data
-repeatWeather = 90 # option to repeat the data in weatherFile x number of times...default value is 1 for no repeat
+weatherFile = tk.StringVar(value="sample_data\RoccaInterp30.xlsx") # default value for the location of the weather data
+repeatWeather = 1 # option to repeat the data in weatherFile x number of times...default value is 1 for no repeat
 resultsFile = tk.StringVar(value = 'sample_output\guitest') # default value for the location where results are saved
 
 capOptions = {"No": HydroNC, "Yes": HydroCap}
 speciesOptions = {"Triticum aestivum": Taest, "Sorghum bicolor": Sbico, "Opuntia ficus-indica": Oficu, "Agave tequilana": Atequ}
 soilOptions = {"Sand": Sand, "Sandy loam": SandyLoam, "Loamy sand": LoamySand, "Loam": Loam, "Clay": Clay}
-rainOptions = {"Drydown":"DRYDWN", "Constant": "CONST", "Input rainfall":"ACTUAL", "Input VWC":"VWC", "Stochastic": "STOCH"}
+rainOptions = {"Drydown": DrydownSoil(), "Constant": ConstantSoil(), "Stochastic rainfall": StochasticSoil(1.5, .3)}
 
 # Take inputs from GUI
 def makemenu(root, caption, rowno, default, *options):
@@ -36,26 +43,26 @@ def makeentry(root, caption, rowno, default):
     entry.grid(row = rowno, column = 1)
 
 def ok():
-    global hydrology, species, sType, locs, duration, s0, rainOption, weatherLoc, resultsLoc
+    global hydrology, species, sType, locs, duration, s0, soilDynamics, weatherLoc, resultsLoc
     duration = durationInp.get()
     s0 = sInit.get()
     hydrology = capOptions[capVariable.get()]
     species = speciesOptions[spVariable.get()]
     sType = soilOptions[soilVariable.get()]
-    rainOption = rainOptions[rainVariable.get()]
+    soilDynamics = rainOptions[rainVariable.get()]
     weatherLoc = weatherFile.get()
     resultsLoc = resultsFile.get()
     root.destroy()
 
 def chooseinputfile():
     global weatherFile
-    root.filename = tkFileDialog.askopenfilename(title = "Select file",filetypes = ( ("Xlsx files","*.xlsx"), ("All files","*")))
+    root.filename = filedialog.askopenfilename(title = "Select file",filetypes = ( ("Xlsx files","*.xlsx"), ("All files","*")))
     weatherFile.set(root.filename)
     print(weatherFile.get())
 
 def chooseoutputfile():
     global resultsFile
-    root.filename = tkFileDialog.asksaveasfilename(title = "Select file",filetypes = (  ("Xlsx files","*.xlsx"), ("All files","*")))
+    root.filename = filedialog.asksaveasfilename(title = "Select file",filetypes = (  ("Xlsx files","*.xlsx"), ("All files","*")))
     resultsFile.set(root.filename)
     print(resultsFile.get())
 
